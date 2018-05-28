f50ee01e99b9d17b877054b19422f3ed81744247
Oworks <o_works@outlook.com>
2018-05-28 11:12:37 +0200

    first commit

---
+++ src/model/hiking-model.ts
@@ -0,0 +1,9 @@
+export class HikingModel {
+
+  constructor(public name: string, public distance: number, public start: string, public end: string){
+    this.name = name;
+    this.distance = distance;
+    this.start = start;
+    this.end = end;
+  }
+}
